d29970ff6a064e12f3f7fd762986303928bbc044
Kurt Silla <kurt@promotexter.com>
2017-12-15 15:02:51 +0800

    more on archiving

--- dist/archieve.js
+++ dist/archieve.js
@@ -1,18 +1,16 @@
+const configs = require("config/config");
 const { spawn } = require('child_process');
-var toDate = new Date();
-toDate.setMilliseconds(toDate.getMilliseconds());
-var to = toDate.toISOString();
+let cfdate = configs.mongo.from;
 var fromDate = new Date();
-fromDate.setMonth(fromDate.getMonth() - 6);
+fromDate.setMonth(fromDate.getMonth() - cfdate);
 var from = fromDate.toISOString();
-var datenow = new Date();
+// var datenow = new Date();
 // console.log(datenow.getMonth());
-// return false;
 console.log('from date ->', from);
-console.log('to date ->', to);
-console.log('command-----', "mongoexport --host test-ui.sendtextnow.com:27017 --db aprm-test --collection transactions --limit 10 --query  '{ \"date\":{\"$gte\":\"" + from + "\",\"$lte\":\"" + to + "\"} }' --out example.json");
+// console.log('to date ->',to);
+console.log('command-----', "--host','test-ui.sendtextnow.com:'+configs.mongo.port+'','--db',''+configs.mongo.database+'', '--collection',''+configs.mongo.collection+'','--out',''+configs.mongo.out+'','--query','{ \\\"date\\\":{\\\"$lte\\\":\\\"\"+from+\"\\\"}}'");
 // const ls = spawn("mongoexport --host test-ui.sendtextnow.com:27017 --db aprm-test --collection transactions --limit 10 --query  '{ \"date\":{\"$gte\":\""+from+"\",\"$lte\":\""+to+"\"} }' --out example.json");
-const ls = spawn('mongoexport', ['--host', 'test-ui.sendtextnow.com:27017', '--db', 'aprm-test', '--collection', 'transactions', '--limit', '10', '--out', 'example.json', '--query', "{ \"date\":{\"$gte\":\"" + from + "\",\"$lte\":\"" + to + "\"} }"]);
+const ls = spawn('mongodump', ['--host', 'test-ui.sendtextnow.com:' + configs.mongo.port + '', '--db', '' + configs.mongo.database + '', '--collection', '' + configs.mongo.collection + '', '--out', '' + configs.mongo.out + '', '--query', "{ \"date\":{\"$lte\":\"" + from + "\"}} "]);
 console.log("exporting...");
 ls.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
---
+++ dist/config/config.js
@@ -0,0 +1,12 @@
+let config = {
+    mongo: {
+        host: '127.0.0.1',
+        port: '27017',
+        collection: 'transactions',
+        database: 'aprm-test',
+        out: "/",
+        from: 6,
+        to: "",
+    },
+};
+module.exports = config;
